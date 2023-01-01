91a251fdeca17027532614cc02eefe81f66a603b
KonohaChen <192732958@qq.com>
2023-01-01 14:28:23 -0500

    1.1

---
+++ res_get.py
@@ -0,0 +1,40 @@
+import requests
+import json
+import os
+
+from os import makedirs
+from os.path import exists
+
+url = "https://api.openai.com/v1/completions"
+
+with open("F:\openai_demo\questions.txt", "r") as q,open("F:\openai_demo\\answer.txt","a") as f:
+    question = q.readline()
+    while question:
+
+        payload = json.dumps({
+          "model": "text-davinci-003",
+          "prompt": question,
+          "max_tokens": 25,
+          "temperature": 0
+        })
+        headers = {
+          'Authorization': 'Bearer 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',
+          'Content-Type': 'application/json'
+        }
+
+        response = requests.request("POST", url, headers=headers, data=payload)
+
+        print(response.text)
+
+        js = json.dumps(response.text)
+
+        answer = json.loads(response.text)
+        text = answer["choices"]
+
+        json.dump(text,f)
+        f.write('\n')
+        print("加载入文件完成...")
+
+        question = q.readline()
+q.close()
+f.close()
